76d35c34938d938dfdcd77cb70729f921ebc6092
Gyovanne <gyovanne@binome.com.br>
2023-08-25 00:19:55 -0300

    :sparkles: create currency table

--- database/migrations/2023_08_24_192500_create_currencies_table.php
+++ database/migrations/2023_08_24_192500_create_currencies_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('currencies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('code');
+            $table->integer('number');
+            $table->integer('decimal')->nullable();
+            $table->string('currency')->nullable();
+            $table->json('currency_locations')->nullable();
             $table->timestamps();
+            $table->unique(['code','number']);
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('currencies');
     }
 };
